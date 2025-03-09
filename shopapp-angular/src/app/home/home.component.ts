f2ef14683ef7a4323d86ae80ef1963d617f7fc2e
1720-Võ Phan Trường Nguyên <nhoxzero191@gmail.com>
2025-03-09 14:05:55 +0700

    update angular order

--- shopapp-angular/src/app/home/home.component.ts
+++ shopapp-angular/src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  imports: [FooterComponent, HeaderComponent]
 })
 export class HomeComponent {
 

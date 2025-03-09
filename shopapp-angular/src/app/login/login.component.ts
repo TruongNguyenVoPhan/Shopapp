340aeeb3013eabf032b574f2685f45083e9fefe9
1720-Võ Phan Trường Nguyên <nhoxzero191@gmail.com>
2025-03-10 02:15:30 +0700

    addeddetail,  login, order-conform , register

---
+++ shopapp-angular/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  imports: [HeaderComponent, FooterComponent]
+})
+export class LoginComponent {
+
+}

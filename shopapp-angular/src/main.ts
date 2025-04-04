211c2aabae75e1380ffdc54b5ba8225b7e834ea5
1720-Võ Phan Trường Nguyên <nhoxzero191@gmail.com>
2025-04-04 16:08:43 +0700

    navigate router angular fe

--- shopapp-angular/src/main.ts
+++ shopapp-angular/src/main.ts
@@ -1,12 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { HomeComponent } from './app/components/home/home.component'; // Đổi từ AppComponent sang HomeComponent
-import { OrderComponent } from './app/components/order/order.component';
-import { OrderDetailComponent } from './app/components/order.detail/order.detail.component';
-import { LoginComponent } from './app/components/login/login.component';
-import { RegisterComponent } from './app/components/register/register.component';
-import { DetailProductComponent } from './app/components/detail-product/detail-product.component';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app/app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)

284c6b51742d42f56684f9f6227ffe3b3f6e5b8d
1720-Võ Phan Trường Nguyên <nhoxzero191@gmail.com>
2026-06-29 17:31:56 +0700

    shopapp v2.03 update prod api url

--- shopapp-angular/src/app/environment/environment.ts
+++ shopapp-angular/src/app/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiBaseUrl: "http://localhost:8088/api/v1/",
+  //apiBaseUrl: "http://localhost:8088/api/v1/",
+  apiBaseUrl: "https://kshopapp-backend.onrender.com/api/v1/",
   production: true
 }
